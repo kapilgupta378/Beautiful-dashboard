0f243884e7b05c7e3cdc9a588ee436b9f2bf9d51
kapil gupta <kapil0506gupta@gmail.com>
2021-09-03 20:15:23 +0530

    chart added at home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
-      {/* <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-      <div className="homeWidgets">
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      {/* <div className="homeWidgets">
         <WidgetSm/>
         <WidgetLg/>
       </div> */}
